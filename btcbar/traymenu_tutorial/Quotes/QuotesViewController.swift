223c885a0a36bb0967060b06c30327414c01c817
Aymon Fournier <ceo@invio.us>
2016-06-20 20:20:49 -0400

    Changes to WebView
    Shrank WebView window, made it take up the entire real-estate of the
    popout.

--- btcbar/traymenu_tutorial/Quotes/QuotesViewController.swift
+++ btcbar/traymenu_tutorial/Quotes/QuotesViewController.swift
@@ -9,16 +9,30 @@
 import Cocoa
 import WebKit
 
-class QuotesViewController: NSViewController {
+class QuotesViewController: NSViewController, WebFrameLoadDelegate {
     @IBOutlet var webView: WebView!
     
-    override func viewDidLoad() {
+    override func viewDidAppear() {
         super.viewDidLoad()
         
-        let url = NSURL (string: "https://bitcoinity.org/markets?theme=dark");
+        let appearance = NSUserDefaults.standardUserDefaults().stringForKey("AppleInterfaceStyle") ?? "Light"
+        
+        
+        let url = appearance == "Light" ? NSURL (string: "https://bitcoinity.org/markets?theme=light") : NSURL (string: "https://bitcoinity.org/markets?theme=dark")
         let requestObj = NSURLRequest(URL: url!);
+        
+        webView.shouldUpdateWhileOffscreen = false
+        webView.frame=self.view.bounds;
+        webView.frameLoadDelegate = self
         webView.mainFrame.loadRequest(requestObj)
 
+    }
+    
+    func webView(sender: WebView, didFinishLoadForFrame frame: WebFrame) {
+        //get the scroll view that contains the frame contents
+        var scrollView: NSScrollView = webView.mainFrame.frameView.documentView.enclosingScrollView!
+        
+        scrollView.hasHorizontalScroller = true
     }
     
 
